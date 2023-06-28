259e49b488e37c2d0d7d4d3460caf853814c61e6
kunaldoliya90 <kunal.doliya90@gmail.com>
2023-06-29 02:02:04 +0530

    Conditionals and loops file added

---
+++ 04_conditionals_loops.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main ()
+{
+    /* 
+    
+    int a = 2;
+    int b = 3;
+    cout<<(a <= b);
+    cout<<(a >= b);
+    cout<<(a == b);
+    cout<<(a != b);
+    // This will give answer in true or false
+
+    */
+
+   /*
+   
+   To check multiple condition we use &&, ||
+   
+   */
+
+
+}
---
+++ generalgitcommands.cpp
@@ -0,0 +1,21 @@
+/*
+
+First Time 
+
+[
+git init
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/kunaldoliya90/CP.git
+git push -u origin main
+]
+
+For updating existing repo on github.
+
+[
+git add .
+git commit -m "Your Message"
+git push
+]
+
+*/
